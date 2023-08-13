38de5b6d9a61e55ac493bab677f911f1341303e9
hugs <h.t.dictus@gmail.com>
2023-08-13 17:14:03 +0200

    rewrite the code to be clearer and cleaner
    need to rewrite pd-ecs toaccomodate it. Specifically, we're direclty
    .loc-ing and indexing the world.
    It should get and set entries as though it is a dataframe, while
    allowing flexibiity and customizability in their storage

--- anasazi/__init__.py
+++ anasazi/__init__.py
@@ -17,23 +17,20 @@ import anasazi.components as comp
 # TODO: if we want integrate arbitrary models, things like max_grain_stock need to be components
 #   or part of the stockpile component
 # then, we need some means to efficiently query the stockpile.
-def harvest_grain(world, max_grain_stock=1600):
+def harvest_corn(world, max_grain_stock=1600):
     """All households harvest grain.
 
     Part of the artificial anasazi model. The annual harvest is sampled from a normal
     distribution where the center is grain_yield['mean'] and scale is grain_yield['var']
     """
-    households = world[(comp.stockpile, comp.farmland)]
-    land = world[comp.grain_yield]
-    harvest_statistics = land.loc[households[comp.farmland]['id']]
+    households = world[[corn_stockpile, farmland]]
+    land = world[[corn_yield_mean, corn_yield_var]]
+    harvest_stats = land.loc[households[farmland], [corn_yield_mean, corn_yield_var]]
     harvest = np.random.normal(
-        harvest_statistics['mean'].values, harvest_statistics['var'].values)
-    grain_stockpile = households[comp.stockpile]
-    grain_stockpile['grain'] = np.clip(
-        grain_stockpile['grain'] + harvest, 0, max_grain_stock)
-    world.update({comp.stockpile: grain_stockpile})
-    return pd.Series(harvest, index=grain_stockpile.index)
-
+        harvest[corn_yield_mean], harvest[corn_yield_var])
+    households[corn_stockpile] += np.clip(harvest, 0, world[max_stockpile])
+    world.loc[households.index, corn_stockpile] = households[corn_stockpile]
+    return pd.Series(harvest, index=households.index)
 
 # We run into a problem: stock-taking can depend on e.g. last harvest
 # which is not something that harvest_grain does not necessarily need to add to state...
@@ -49,10 +46,14 @@ def stock_taking(world, harvest):
 
     harvest: a series containing the size of the last harvest
     """
-    food_needs, stockpiles = world[(comp.food_needs, comp.stockpile)].data()
+    households = world[[food_needs, corn_stockpile]]
+    expects_to_starve = households[food_needs] > (households[corn_stockpile] + harvest)
     # TODO: this assumes that households corresponds to harvest, which is not always guaranteed.
     expects_to_starve = food_needs['grain'] > stockpiles['grain'] + harvest
-    world.give(expects_to_starve.index[expects_to_starve], {comp.moving: {}})
+    # TODO: this is increasingly indicating that these need to be methods of an object,
+    #    one with access to world.
+    # perhaps world itself.
+    move(world, households[expects_to_starve].index())
 
 # TODO: a lot of these questions come down to: when should something be passed from function to function, and when should it be stored in the world state.
 # Ultimately this depends on whether taht value will be used in the future...
@@ -69,88 +70,68 @@ def stock_taking(world, harvest):
 # there will be operations here that do not involve any "game logic", that just adjust the other components that need to change when an entity moves house...
 
 def _move_out(world, movers):
-    havefarms = world[(comp.moving, comp.farmland, comp.home)]
-    _, farm_ids, home_ids = havefarms.data()
-    farms = farm_ids['id'].value_counts()
-    homes = home_ids['id'].value_counts()
-    world[comp.occupying_farms].loc[farms.index, 'num occupants'] -=\
-        farms.values
-    world[comp.occupying_houses].loc[homes.index, 'num occupants'] -=\
-        homes.values
+    world[occupying_farms].loc[world.loc[movers, farmland]] -= 1
+    world[occupying_homes].loc[world.loc[movers, home]] -= 1
+    del world.loc[movers, [farmland, home]]
 
+def _find_farm(world, movers):
+    # TODO: we should use numba.njit for this tbh
+    # TODO: or a matching algorithm, only then it's not strictly speaking artifical anasazi...
+    unoccupied_land = world[[grain_yield, position, ~occupying_houses, ~farmed]].index
+    distances = _calculate_distances_matrix(
+        world.loc[movers, [position]]
+        world.loc[land.index, position])
+    enough_to_survive = world.loc[unoccupied_land, mean_corn_yield].values[np.newaxis, ...]\
+        >= world.loc[movers, food_needs].values[..., np.newaxis]
+    distances[~enough_to_survive] == np.inf
 
-def _find_home(mover_positions, mover_needs, arable_land):
-    positions, yields, occupation, houses = arable_land.data()
-    unoccupied = np.logical_and(
-        occupation['num occupants'] == 0,
-        houses['num occupants'] == 0)
-    positions = positions[unoccupied]
-    yields = yields[unoccupied]
-    displacements = (
-        positions.values[np.newaxis, ...]
-        - mover_positions.values[..., np.newaxis, :]
-    )
-    distances = np.linalg.norm(displacements, axis=-1)
-    enough_to_survive = (
-        yields['mean'].values[np.newaxis, ...]
-        >= mover_needs['grain'].values[..., np.newaxis]
-    )
-    distances[~enough_to_survive] = np.inf
     nearest_farms = []
+
     for dist in distances:
         if not np.isfinite(dist).any():
             # assert False, "lol, TODO"
             pass
         nearest = np.argmin(dist)
         distances[:, nearest] = np.inf
-        nearest_farms.append(positions.index[nearest])
-    return mover_needs.index, nearest_farms
+        nearest_farms.append(unoccupied_land[nearest])
+    return movers, nearest_farms
 
 
-def _move_in(world, household_ids, farm_ids, farm_positions):
-    world.give(household_ids, {comp.farmland: {'id': farm_ids}})
-    world[comp.occupying_farms].loc[farm_ids] += 1
-    # TODO: there is a repeated operation here I should abstract away
-    # TODO: should there maybe be a filter???
-    positions = farm_positions.loc[farm_ids]
-    potential_housing = world[(comp.position, comp.occupying_houses, comp.occupying_farms)]
-    house_posns, occupants, farmed = potential_housing.data()
+def _start_farm(world, mover_ids, farm_ids):
+    world.loc[households, farmland] = farm_ids
+    world.loc[farm_ids, farmed] = household_ids
 
-    potential_house_positions = house_posns[farmed['num occupants'] == 0]
-    house_to_farm_distance = np.linalg.norm(
-        potential_house_positions.values[np.newaxis, ...]
-        - positions.loc[farm_ids].values[..., np.newaxis, :],
-        axis=-1
-        )
+
+def _find_home(world, mover_ids, farm_ids):
+    potential_housing = world[[position, occupying_houses, ~farmed]]
+    
+    house_to_farm_distance = _calculate_distance_matrix(
+        potential_housing[position],
+        - world.loc[farm_ids, position].values[..., np.newaxis, :])
     houses = potential_house_positions.iloc[
-        np.argmin(house_to_farm_distance, axis=1)]
+        np.argmin(house_to_farm_distance, axis=1)
+    ]
 
     house_ids, nhouses = np.unique(houses.index, return_counts=True)
-    occupants.loc[house_ids, 'num occupants'] += nhouses
+    return houses.index
+
+
+def _move_in(world, household_ids, house_ids):
+    unique_ids, house_counts = np.unique(house_ids, return_counts=True)
+    world.loc[unique_ids, occupying_homes] += house_counts
     # there are too many different ways to update the world state
     # this does not make it especially simple
     # getitem is really simple... maybe we should just stick with that?
-
-    world[comp.position].loc[household_ids] = house_posns.loc[house_ids].values
-    world.give(household_ids, {comp.home: {'id': house_ids}})
-    world.update({comp.occupying_houses: occupants})
+    world.loc[household_ids, position] = world.loc[house_ids, position]
+    world.loc[household_ids, home] = house_ids
 
 
-def moving_house(world):
+def moving_house(world, mover_ids):
     """Moving households seek a home that can feed them."""
-    # TODO: I can't entirely explain why, but I have a feeling that it would be a good
-    # practice to not have helper functions push changes.
-    movers = world[
-        (comp.food_needs, comp.moving, comp.position)
-    ]
-    needs, moving, position = movers.data()
-    # move out: see this is a fairly generic operation that many systems will need...
     _move_out(world, movers.ids)
-    arable_land = world[
-        (comp.position, comp.grain_yield,
-         comp.occupying_farms, comp.occupying_houses)
-    ]
-    ids, farms = _find_home(position, needs, arable_land)
+    mover_ids, farm_ids = _find_farm(world, mover_ids)
+    _start_farm(world, mover_ids, farm_ids)
+    _find_home(world, mover_ids, farm_ids)
     _move_in(world, ids, farms, world[comp.position])
 
 
